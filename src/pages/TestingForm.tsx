ac6f9264b2fffc6f6739558ab7767dcb30ef8a5f
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-09-02 17:52:15 +0500

    Check again

--- src/pages/TestingForm.tsx
+++ src/pages/TestingForm.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const TestingForm = () => {
+    
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // handle form data here
+    console.log("Form submitted!")
+  }
     return (
         <div>
-            <form name='contact v1' method='post' data-netlify='true' onSubmit={"submit"} >
+            <form name='contact v1' method='post' data-netlify='true' onSubmit={handleSubmit} >
                 <input type="hidden" name="form-name" value={"contact v1"} />
                 <div>
                     <label htmlFor="name">Name</label>
